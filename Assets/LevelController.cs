d76c87a001e7fb566e2c7092f0dfbda5071d3c30
CyborgGit <ruddy.pogodin@gmail.com>
2023-07-11 13:44:24 +0200

    Some small updates
    Added effect for button to go lower if pressed. Added timer on canvas(yet without high score). Started my level. Fixed level load problem.

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -8,13 +8,16 @@ public class LevelController : MonoBehaviour
     [SerializeField] Material playerMaterial;
     [SerializeField] Texture[] textures;
     [SerializeField] TextMeshProUGUI itemsText;
+    [SerializeField] TextMeshProUGUI timeText;
 
     int totalItemCount = 0;
     int itemCount = 0;
+    float ttime;
 
     // Start is called before the first frame update
     void Start()
     {
+        timeText.text = "0";
         playerMaterial.mainTexture = textures[Globals.textureIndex];
 
         foreach (GameObject itemGo in GameObject.FindGameObjectsWithTag("Item")) {
@@ -44,6 +47,7 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ttime = Time.time;
+        timeText.text = ttime.ToString("F2");
     }
 }
