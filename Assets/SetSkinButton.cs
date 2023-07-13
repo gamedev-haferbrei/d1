b9d448b8ee769d830de5a66afd3095173d5874f0
CyborgGit <ruddy.pogodin@gmail.com>
2023-07-13 10:05:09 +0200

    Menu update
    Added effect on skin selection

--- Assets/SetSkinButton.cs
+++ Assets/SetSkinButton.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetSkinButton : MonoBehaviour
 {
     [SerializeField] int textureIndex;
+    [SerializeField] Image[] image;
 
     public void SetTexture()
     {
         Globals.textureIndex = textureIndex;
+    }
+
+    public void Selector()
+    {
+        for (int i = 0; i < image.Length; i++)
+        {
+            if (i == textureIndex)
+            {
+                image[i].color = Color.white;
+            }
+            else
+            {
+                image[i].color = Color.clear;
+            }
+        }
     }
 }
